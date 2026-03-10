a63d9291ed2ce5d80a2fb2f57d77dec4b6b18a9b
Leofernandy <leofernandyabc@gmail.com>
2026-03-10 20:25:49 +0700

    add bye

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to Leo's CI/CD Pipeline! and Thankyou"
+    return "Welcome to Leo's CI/CD Pipeline! and Thankyou! Bye"
 
 @app.route("/status")
 def status():
--- test_app.py
+++ test_app.py
@@ -1,7 +1,7 @@
 import app
 
 def test_home():
-    assert app.home() == "Welcome to Leo's CI/CD Pipeline! and Thankyou"
+    assert app.home() == "Welcome to Leo's CI/CD Pipeline! and Thankyou! Bye"
 
 def test_status():
     assert app.status() == "OK - System is running smoothly"

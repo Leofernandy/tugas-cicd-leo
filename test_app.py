b59f22395a44cfd1f0d8c5813387813eb7e68c16
Leofernandy <leofernandyabc@gmail.com>
2026-03-10 20:09:22 +0700

    add thankyou

--- test_app.py
+++ test_app.py
@@ -1,7 +1,7 @@
 import app
 
 def test_home():
-    assert app.home() == "Welcome to Leo's CI/CD Pipeline!"
+    assert app.home() == "Welcome to Leo's CI/CD Pipeline! and Thankyou"
 
 def test_status():
     assert app.status() == "OK - System is running smoothly"
